780391210807b4c9f642d5383c38f3e554259775
Chris Kolkman <ckolkman@treorisoft.com>
2021-10-01 06:31:42 -0400

    remove unused function

--- packages/querystring/src/isObjEmpty.ts
+++
@@ -1,8 +0,0 @@
-
-export function isObjEmpty(value: unknown): boolean {
-  if (typeof value === 'undefined') return true;
-  if (value === null) return true;
-  if (Array.isArray(value) && !value.length) return true;
-  if (typeof value === 'object' && Object.keys(value!).length) return true;
-  return false;
-}
